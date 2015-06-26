8e6360118d9a93e3936a5ba2edfc0f88cab28ac7
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-06-26 21:01:40 +0200

    Various improvements in pluc/custom.el

--- pluc/custom.el
+++ pluc/custom.el
@@ -9,11 +9,13 @@
  show-trailing-whitespace t
  inhibit-startup-screen t                                ;; Do not show the welcome message
  initial-scratch-message nil
- message-log-max 50                                      ;; Disable off message buffer
+ message-log-max 200                                     ;; Disable off message buffer
  visible-bell nil                                        ;; Get rid of bells
  ring-bell-function 'ignore
  vc-follow-symlinks t                                    ;; Automatically follow symlinks to files under CVS
  uniquify-buffer-name-style 'post-forward-angle-brackets ;; Identify multiple buffers with the same file name
+ kill-ring-max 500 ;; Kill-ring capacity
+ kill-whole-line t ;; When killing a whole line, also remove the terminating newline
  )
 
 (menu-bar-mode 0)        ;; Remove menu
@@ -24,6 +26,8 @@
 (line-number-mode 1)     ;; Display current line
 (column-number-mode 1)   ;; Display current column
 (display-battery-mode 0) ;; Disable battery mode
+(prefer-coding-system 'utf-8) ;; Primary coding system for automatic detection.
+(set-language-environment "UTF-8") ;; Default input method
 
 ;; Allow undo/redo windows configuration
 (when (fboundp 'winner-mode)
