eafcab88f3a60da676453d8b30a407f19984a86e
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-06-28 00:00:28 +0200

    Enable delete-selection-mode. Use newline-and-indent. Pretty comment boxes in editing.el.

--- pluc/editing.el
+++ pluc/editing.el
@@ -1,20 +1,39 @@
-;; Common editing settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Common editing settings ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Increase selected region by semantic units
-;; https://github.com/magnars/expand-region.el
+(delete-selection-mode 1) ;; Typing replaces current selection
+
+;;;;;;;;;;;;;;;;;;
+;; Key bindings ;;
+;;;;;;;;;;;;;;;;;;
+
+(use-package bind-key
+  :bind
+  ("RET" . newline-and-indent) ;; Indent on new line
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Increase selected region by semantic units  ;;
+;; https://github.com/magnars/expand-region.el ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package expand-region
   :bind
   ("C-a" . er/expand-region)
   ("M-a" . er/contract-region))
 
-;; Drag stuff (lines, words, region, etc...) around
-;; https://github.com/rejeep/drag-stuff.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Drag stuff (lines, words, region, etc...) around ;;
+;; https://github.com/rejeep/drag-stuff.el	    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package drag-stuff
   :config
   (drag-stuff-global-mode t))
 
-;; Visual feedback on yanks, undo, etc
-;; http://www.emacswiki.org/emacs/VolatileHighlights
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Visual feedback on yanks, undo, etc		     ;;
+;; http://www.emacswiki.org/emacs/VolatileHighlights ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package volatile-highlights
   :config
   (volatile-highlights-mode t))
