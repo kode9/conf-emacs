724f5f6bab26a9211e5303786467d5b76b70f15f
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-07-03 20:26:55 +0200

    Add key bindings for ag

--- pluc/pluc-tools.el
+++ pluc/pluc-tools.el
@@ -7,6 +7,22 @@
   :init
   (setq ag-highlight-search t) ;; Highlight search terms
   (setq ag-reuse-buffer t) ;; Use a single buffer
+  :bind*
+  ;; Search STRING in DIR
+  ("C-c a a" . ag)
+  ("C-c a A" . ag-project)
+  ;; Search REGEX in DIR
+  ("C-c a r" . ag-regexp)
+  ("C-c a R" . ag-project-regexp)
+  ;; Search STRING in DIR, limited FILE TYPES
+  ("C-c a f" . ag-files)
+  ("C-c a F" . ag-project-files)
+  ;; Find FILES in DIR
+  ("C-c A d" . ag-dired)
+  ("C-c A D" . ag-project-dired)
+  ;; Find FILES matching REGEX in DIR
+  ("C-c A r" . ag-dired-regexp)
+  ("C-c A R" . ag-project-regexp)
   )
 
 (provide 'pluc-tools)
