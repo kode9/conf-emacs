65b39ca305b84c7572a44728cc8334021f6929fb
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-11-03 00:48:11 +0100

    Update fonts, specify per-font size for monospace.

--- pluc/pluc-custom.el
+++ pluc/pluc-custom.el
@@ -37,23 +37,32 @@
 (use-package cus-edit :defer t :config (setq custom-file pluc-custom-file) :init (load pluc-custom-file 'no-error 'no-message))
 
 ;; Fonts
+;;; I'd like to try out "Fira Code" when Emacs get ligatures support
 (use-package dynamic-fonts
   :init
   (setq
-   dynamic-fonts-preferred-monospace-fonts '(
-					     ;; "Fira Code" ;; Needs ligatures support
-					     "Inconsolata"
-					     "Meslo LG L DZ"
-					     "Fira Mono"
-					     "Envy Code R"
-					     "Dejavu Sans Mono"
-					     "Droid Sans Mono"
-					     "Liberation Mono"
-					     "Cousine"
-					     "BPmono"
-					     )
-   dynamic-fonts-preferred-monospace-point-size 12
-   dynamic-fonts-preferred-proportional-point-size 12)
+   dynamic-fonts-set-alternatives t ;; Fill up alternative fonts
+   dynamic-fonts-preferred-monospace-fonts '("Hack-10"
+					     "Meslo LG S DZ-10"
+					     "Envy Code R-11"
+					     "Fira Mono-10"
+					     "Inconsolata-11"
+					     "Droid Sans Mono-10"
+					     "Dejavu Sans Mono-10"
+					     "Cousine-10"
+					     "Liberation Mono-10"
+					     "Anonymous Pro-11"
+					     "BPmono-10")
+   dynamic-fonts-preferred-monospace-point-size nil
+   dynamic-fonts-preferred-proportional-fonts '("Open Sans"
+						"Roboto"
+						"Dejavu Sans"
+						"Arimo"
+						"Droid Sans"
+						"Liberation Sans"
+						"Verdana"
+						"Arial")
+   dynamic-fonts-preferred-proportional-point-size 11)
   :config (dynamic-fonts-setup))
 
 ;; Remote access
