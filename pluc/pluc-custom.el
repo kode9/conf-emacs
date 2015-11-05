557d0abe071fb7483277f3c639341d271c47e1bc
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-11-05 23:46:49 +0100

    Proper comments in pluc-custom.el

--- pluc/pluc-custom.el
+++ pluc/pluc-custom.el
@@ -1,35 +1,39 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Basic configuration ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; pluc-custom.el --- Basic configuration           -*- lexical-binding: t; -*-
+;;
+;;
+;; Author: Pierre-Luc Perrier <pluc@the-pluc.net>
+;;
+;;; Commentary:
+;;
+;;; Code:
 
 (setq
- frame-title-format "⸗ %b (%&) ⸗" ;; Frame title format: buffer (status)
+ frame-title-format "⸗ %b (%&) ⸗"				 ; Frame title format: buffer (status)
  display-time-24hr-format nil
  display-time-day-and-date nil
- ;; mouse-avoidance-mode (quote cat-and-mouse) nil (avoid)
  python-python-command "python3"
  show-trailing-whitespace t
- inhibit-startup-screen t                                ;; Do not show the welcome message
+ inhibit-startup-screen t					 ; Do not show the welcome message
  initial-scratch-message nil
- message-log-max 200                                     ;; Disable off message buffer
- visible-bell nil                                        ;; Get rid of bells
+ message-log-max 200						 ; Disable off message buffer
+ visible-bell nil						 ; Get rid of bells
  ring-bell-function 'ignore
- vc-follow-symlinks t                                    ;; Automatically follow symlinks to files under CVS
- uniquify-buffer-name-style 'post-forward-angle-brackets ;; Identify multiple buffers with the same file name
- kill-ring-max 500 ;; Kill-ring capacity
- kill-whole-line t ;; When killing a whole line, also remove the terminating newline
+ vc-follow-symlinks t						 ; Automatically follow symlinks to files under CVS
+ uniquify-buffer-name-style 'post-forward-angle-brackets	 ; Identify multiple buffers with the same file name
+ kill-ring-max 500						 ; Kill-ring capacity
+ kill-whole-line t						 ; When killing a whole line, also remove the terminating newline
  )
 
-(menu-bar-mode 0)        ;; Remove menu
-(tool-bar-mode 0)        ;; Remove toolbar
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode 0)) ;; No scrollbar
-(display-time-mode 0)    ;; Hide current time
-(size-indication-mode 0) ;; Hide buffer size
-(line-number-mode 1)     ;; Display current line
-(column-number-mode 1)   ;; Display current column
-(display-battery-mode 0) ;; Disable battery mode
-(prefer-coding-system 'utf-8) ;; Primary coding system for automatic detection.
-(set-language-environment "UTF-8") ;; Default input method
+(menu-bar-mode 0)					 ; Remove menu
+(tool-bar-mode 0)					 ; Remove toolbar
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))	 ; No scrollbar
+(display-time-mode 0)					 ; Hide current time
+(size-indication-mode 0)				 ; Hide buffer size
+(line-number-mode 1)					 ; Display current line
+(column-number-mode 1)					 ; Display current column
+(display-battery-mode 0)				 ; Disable battery mode
+(prefer-coding-system 'utf-8)				 ; Primary coding system for automatic detection.
+(set-language-environment "UTF-8")			 ; Default input method
 
 ;; Customization
 ;;; Took from https://github.com/lunaryorn/.emacs.d
@@ -48,18 +52,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq
- savehist-file (expand-file-name ".history" user-emacs-directory) ;; History file
- auto-save-default nil ;; Disable autosave for now
- backup-inhibited t ;; Disable backup for now
+ savehist-file (expand-file-name ".history" user-emacs-directory)	 ; History file
+ auto-save-default nil							 ; Disable autosave for now
+ backup-inhibited t							 ; Disable backup for now
  )
-(savehist-mode nil) ;; save minibuffer history
+(savehist-mode nil)							 ; save minibuffer history
 
 ;; Gets the mouse out of the cursor
 (use-package avoid
   :init
-  (setq mouse-avoidance-threshold 10) ;; When is it too close
+  (setq mouse-avoidance-threshold 10)	 ; When is it too close
   :config
-  (mouse-avoidance-mode 'exile) ;; Exile it to top-right corder, but allow it to come back
+  (mouse-avoidance-mode 'exile)		 ; Exile it to top-right corder, but allow it to come back
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -82,8 +86,8 @@
   :disabled t
   :diminish golden-ratio-mode
   :init
-  (setq golden-ratio-auto-scale nil) ;; If not nil, keep frames narrow on wide screens
-  (setq split-width-threshold nil) ;; Prevent additional windows creation
+  (setq golden-ratio-auto-scale nil)	 ; If not nil, keep frames narrow on wide screens
+  (setq split-width-threshold nil)	 ; Prevent additional windows creation
   :config
   (golden-ratio-mode 1))
 
@@ -94,9 +98,10 @@
 (use-package paradox
   :defer t
   :init
-  (setq paradox-github-token t) ;; Don't ask for GitHub token
-  (setq paradox-execute-asynchronously nil) ;; Don't try to do things asynchronously
-  (setq paradox-automatically-star nil) ;; Do not star automatically when (un)installing
+  (setq paradox-github-token t)			 ; Don't ask for GitHub token
+  (setq paradox-execute-asynchronously nil)	 ; Don't try to do things asynchronously
+  (setq paradox-automatically-star nil)		 ; Do not star automatically when (un)installing
   )
 
 (provide 'pluc-custom)
+;;; pluc-custom.el ends here
