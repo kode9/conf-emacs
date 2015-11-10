425622a42c4631da799bb545df796a46d7ff55b3
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-11-10 23:47:42 +0100

    Clean pluc-tools.el

--- pluc/pluc-tools.el
+++ pluc/pluc-tools.el
@@ -1,12 +1,17 @@
-;; External tools integration
+;;; pluc-tools.el --- External tools integration           -*- lexical-binding: t; -*-
+;;
+;;
+;; Author: Pierre-Luc Perrier <pluc@the-pluc.net>
+;;
+;;; Commentary:
+;;
+;;; Code:
 
-;; ag the silver searcher
-;;; https://github.com/ggreer/the_silver_searcher
-;;; https://github.com/Wilfred/ag.el
+;; ag the silver searcher: a better grep alternative
 (use-package ag
   :init
-  (setq ag-highlight-search t) ;; Highlight search terms
-  (setq ag-reuse-buffer t) ;; Use a single buffer
+  (setq ag-highlight-search t) ; Highlight search terms
+  (setq ag-reuse-buffer t)     ; Use a single buffer
   :bind*
   ;; Search STRING in DIR
   ("C-c a a" . ag)
@@ -25,8 +30,7 @@
   ("C-c A R" . ag-project-regexp)
   )
 
-;; magit: Git frontend
-;;; http://magit.vc/
+;; magit: (awesome) git frontend
 (use-package magit
   :bind
   ("C-c m s" . magit-status)
@@ -35,35 +39,36 @@
   ("C-c m l" . magit-log-buffer-file)
   ("C-c m L" . magit-log-all)
   :config
-  (setq magit-revert-buffers 'silent) ;; Revert buffers silently
-  (setq magit-save-repository-buffers t) ;; Ask confirmation when saving buffers
-  (setq magit-refs-show-commit-count 'all) ;; Show counts for branches and tags
+  (setq magit-revert-buffers 'silent)      ; Revert buffers silently
+  (setq magit-save-repository-buffers t)   ; Ask confirmation when saving buffers
+  (setq magit-refs-show-commit-count 'all) ; Show counts for branches and tags
   ;; See issue https://github.com/magit/magit/issues/1970
   ;; And PR https://github.com/magit/magit/pull/1956
   (defun pluc-magit-insert-repo-header ()
     "Insert a header line showing the path to the repository top-level."
     (let ((topdir (magit-toplevel)))
       (magit-insert-section (repo topdir)
-	(magit-insert (format "%-10s%s\n" "Repo: "
-			      (abbreviate-file-name topdir))))))
+        (magit-insert (format "%-10s%s\n" "Repo: "
+                              (abbreviate-file-name topdir))))))
   (defun pluc-magit-insert-user-header ()
     "Insert a header line about the current user."
     (let ((name (magit-get "user.name"))
-	  (email (magit-get "user.email")))
+          (email (magit-get "user.email")))
       (when (and name email)
-	(magit-insert-section (user name)
-	  (magit-insert
-	   (concat (format "%-10s" "User: ")
-		   (propertize name 'face 'magit-log-author)
-		   " <" email ">" "\n"))))))
+        (magit-insert-section (user name)
+          (magit-insert
+           (concat (format "%-10s" "User: ")
+                   (propertize name 'face 'magit-log-author)
+                   " <" email ">" "\n"))))))
   ;; Status header format
   (setq magit-status-headers-hook '(pluc-magit-insert-user-header
-				    pluc-magit-insert-repo-header
-				    magit-insert-remote-header
-				    magit-insert-tags-header
-				    magit-insert-head-header
-				    magit-insert-upstream-header))
-  ;; Status buffer name format
-  (setq magit-status-buffer-name-format "*magit-status＠%b*"))
+                                    pluc-magit-insert-repo-header
+                                    magit-insert-remote-header
+                                    magit-insert-tags-header
+                                    magit-insert-head-header
+                                    magit-insert-upstream-header))
+  (setq magit-status-buffer-name-format "*magit-status＠%b*"); Status buffer name format
+  )
 
 (provide 'pluc-tools)
+;;; pluc-tools.el ends here
