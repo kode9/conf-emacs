d0aa02822786dfa468b5d162078f3da0ce89d91c
Pierre-Luc Perrier <pluc@the-pluc.net>
2016-04-27 00:05:08 +0200

    Cleanup dynamic-font loading

--- pluc/pluc-theme.el
+++ pluc/pluc-theme.el
@@ -5,36 +5,20 @@
 (use-package zenburn-theme :demand)
 
 ;; Fonts
-;;; I'd like to try out "Fira Code" when Emacs get ligatures support
 (use-package dynamic-fonts
-  :demand
   :init
-  (setq
-   dynamic-fonts-set-alternatives t ;; Fill up alternative fonts
-   dynamic-fonts-preferred-monospace-fonts '("Hack-10"
-					     "Meslo LG S DZ-10"
-					     "Envy Code R-11"
-					     "Fira Mono-10"
-					     "Inconsolata-11"
-					     "Droid Sans Mono-10"
-					     "Dejavu Sans Mono-10"
-					     "Cousine-10"
-					     "Liberation Mono-10"
-					     "Anonymous Pro-11"
-					     "BPmono-10")
-   dynamic-fonts-preferred-monospace-point-size nil
-   dynamic-fonts-preferred-proportional-fonts '("Open Sans"
-						"Roboto"
-						"Dejavu Sans"
-						"Arimo"
-						"Droid Sans"
-						"Liberation Sans"
-						"Verdana"
-						"Arial")
-   dynamic-fonts-preferred-proportional-point-size 11)
-  :config (dynamic-fonts-setup))
+  (customize-set-variable 'dynamic-fonts-set-alternatives t) ; Fill up alternative fonts
+  (customize-set-variable 'dynamic-fonts-preferred-monospace-point-size 10)
+  (customize-set-variable 'dynamic-fonts-preferred-monospace-fonts
+                          '("Hack" "Dejavu Sans Mono" "courier" "fixed"))
+  (customize-set-variable 'dynamic-fonts-preferred-proportional-fonts
+                          '("Open Sans" "Roboto" "Dejavu Sans" "arial" "fixed"))
+  (customize-set-variable 'dynamic-fonts-preferred-proportional-point-size 10)
+  :config
+  (dynamic-fonts-setup))
 
 (use-package spaceline-config
   :config (spaceline-emacs-theme))
 
 (provide 'pluc-theme)
+;;; pluc-theme.el ends here
