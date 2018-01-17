15fad6d6308075d12d0ec0834e77b451e425c359
Pierre-Luc Perrier <dev@the-pluc.net>
2018-01-17 23:27:05 +0100

    Change default font

--- pluc/pluc-theme.el
+++ pluc/pluc-theme.el
@@ -1,6 +1,26 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Color theme configuration ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; pluc-theme.el --- Fonts & Faces -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015-2018 Pierre-Luc Perrier
+
+;; Author: Pierre-Luc Perrier <dev@the-pluc.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (use-package zenburn-theme :demand)
 
@@ -8,9 +28,9 @@
 (use-package dynamic-fonts
   :init
   (customize-set-variable 'dynamic-fonts-set-alternatives t) ; Fill up alternative fonts
-  (customize-set-variable 'dynamic-fonts-preferred-monospace-point-size 10)
+  (customize-set-variable 'dynamic-fonts-preferred-monospace-point-size 8)
   (customize-set-variable 'dynamic-fonts-preferred-monospace-fonts
-                          '("Hack" "Dejavu Sans Mono" "courier" "fixed"))
+                          '("Monoid" "Hack" "Dejavu Sans Mono" "courier" "fixed"))
   (customize-set-variable 'dynamic-fonts-preferred-proportional-fonts
                           '("Open Sans" "Roboto" "Dejavu Sans" "arial" "fixed"))
   (customize-set-variable 'dynamic-fonts-preferred-proportional-point-size 10)
