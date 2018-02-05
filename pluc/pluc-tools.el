7d732af526abe45cffbf6e98b539573708fa4b55
Pierre-Luc Perrier <dev@the-pluc.net>
2018-02-05 20:08:00 +0100

    Load package `pass`

--- pluc/pluc-tools.el
+++ pluc/pluc-tools.el
@@ -1,10 +1,25 @@
-;;; pluc-tools.el --- External tools integration           -*- lexical-binding: t; -*-
-;;
-;;
-;; Author: Pierre-Luc Perrier <pluc@the-pluc.net>
-;;
+;;; pluc-tools.el --- External tools integration -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015-2018 Pierre-Luc Perrier
+
+;; Author: Pierre-Luc Perrier <dev@the-pluc.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
 ;;; Commentary:
+
 ;;
+
 ;;; Code:
 
 ;; Remote access
@@ -89,6 +104,8 @@
                                     magit-insert-head-header))
   (setq magit-status-buffer-name-format "*magit-status＠%b*"); Status buffer name format
   )
+
+(use-package pass :defer t)
 
 (provide 'pluc-tools)
 ;;; pluc-tools.el ends here
