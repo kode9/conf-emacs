eb4a7faf144676e27e2f2227289c0ae516f42bc5
Pierre-Luc Perrier <dev@the-pluc.net>
2018-02-05 20:07:05 +0100

    Disable package `drag-stuff`

--- pluc/pluc-editing.el
+++ pluc/pluc-editing.el
@@ -1,9 +1,25 @@
-;;; pluc-editing.el --- General edition configuration             -*- lexical-binding: t; -*-
-;;
-;; Author: Pierre-Luc Perrier <pluc@the-pluc.net>
-;;
+;;; pluc-editing.el --- Editing configuration -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015-2018 Pierre-Luc Perrier
+
+;; Author: Pierre-Luc Perrier <dev@the-pluc.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
 ;;; Commentary:
+
 ;;
+
 ;;; Code:
 
 ;;;###autoload
@@ -75,6 +91,7 @@
 ;; https://github.com/rejeep/drag-stuff.el          ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package drag-stuff
+  :disabled t
   :diminish drag-stuff-mode
   :config
   (drag-stuff-global-mode t))
