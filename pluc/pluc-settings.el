7c0755c0df6daf0928498b3f1c0b369c281606b7
Pierre-Luc Perrier <dev@the-pluc.net>
2018-02-06 23:11:20 +0100

    Use defcustom for some settings

--- pluc/pluc-settings.el
+++ pluc/pluc-settings.el
@@ -1,17 +1,39 @@
-;;; pluc-settings.el --- Misc elisp settings           -*- lexical-binding: t; -*-
-;;
-;;
-;; Author: Pierre-Luc Perrier <pluc@the-pluc.net>
-;;
+;;; pluc-settings.el --- Config customization -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017-2018 Pierre-Luc Perrier
+
+;; Author: Pierre-Luc Perrier <dev@the-pluc.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
 ;;; Commentary:
+
 ;;
+
 ;;; Code:
 
-(eval-and-compile
-  (defconst pluc-cache-dir (expand-file-name ".cache" user-emacs-directory)
-    "Directory used to store the different cached files.")
-  (defconst pluc-custom-file (expand-file-name "custom.el" pluc-cache-dir)
-    "File used to store settings from Customization UI."))
+;;;###autoload
+(defcustom pluc-cache-dir (expand-file-name ".cache" user-emacs-directory)
+  "Directory used to store runtime cache files."
+  :type 'directory
+  :group 'pluc)
+
+;;;###autoload
+(defcustom pluc-custom-file (expand-file-name "custom.el" pluc-cache-dir)
+  "File used to store settings from Customization UI."
+  :set-after '(pluc-cache-dir)
+  :type 'file
+  :group 'pluc)
 
 (ignore-errors (make-directory pluc-cache-dir))
 
