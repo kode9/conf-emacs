610058b2d7d5b2b01c4168f92b5656478744ab8e
Pierre-Luc Perrier <dev@the-pluc.net>
2018-11-03 22:00:43 +0100

    Disable `tooltip-mode`

--- pluc/pluc-custom.el
+++ pluc/pluc-custom.el
@@ -1,10 +1,25 @@
 ;;; pluc-custom.el --- Basic configuration           -*- lexical-binding: t; -*-
-;;
-;;
-;; Author: Pierre-Luc Perrier <pluc@the-pluc.net>
-;;
+
+;; Copyright (C) 2015-2018 Pierre-Luc Perrier
+
+;; Author: Pierre-Luc Perrier <dev@the-pluc.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
 ;;; Commentary:
+
 ;;
+
 ;;; Code:
 
 (require 'pluc-settings)
@@ -46,6 +61,7 @@
   (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))                            ; No menu
   (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))                            ; No toolbar
   (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))                        ; No scrollbar
+  (when (fboundp 'tooltip-mode) (tooltip-mode -1))                              ; Tooltips in minibuffer
   (when (fboundp 'display-time-mode) (display-time-mode -1))                    ; No time / load / mail in modeline
   (when (fboundp 'size-indication-mode) (size-indication-mode -1))              ; No buffer size in modeline
   (when (fboundp 'line-number-mode) (line-number-mode nil))                     ; Display current line in modeline
