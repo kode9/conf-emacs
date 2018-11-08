ecefc9cccf6172a6beb7f8b1d78d939e219153f0
Pierre-Luc Perrier <dev@the-pluc.net>
2018-11-08 23:47:10 +0100

    Handle defer in `pluc-theme.el` use-package declarations

--- pluc/pluc-theme.el
+++ pluc/pluc-theme.el
@@ -22,24 +22,36 @@
 
 ;;; Code:
 
-(use-package zenburn-theme)
+(use-package zenburn-theme
+  :demand t)
 
 ;; Fonts
 (use-package dynamic-fonts
+  :functions dynamic-fonts-setup
   :init
   (customize-set-variable 'dynamic-fonts-set-alternatives t) ; Fill up alternative fonts
   (customize-set-variable 'dynamic-fonts-preferred-monospace-point-size 8)
   (customize-set-variable 'dynamic-fonts-preferred-monospace-fonts
-                          '("Monoisome" "Monoid" "Hack" "Dejavu Sans Mono" "courier" "fixed"))
+                          '("Monoisome"
+                            "Monoid"
+                            "Hack"
+                            "Dejavu Sans Mono"
+                            "courier"
+                            "fixed"))
   (customize-set-variable 'dynamic-fonts-preferred-proportional-fonts
-                          '("Open Sans" "Roboto" "Dejavu Sans" "arial" "fixed"))
+                          '("Open Sans"
+                            "Roboto"
+                            "Dejavu Sans"
+                            "arial"
+                            "fixed"))
   (customize-set-variable 'dynamic-fonts-preferred-proportional-point-size 10)
-  :config
-  (dynamic-fonts-setup))
+  :hook (after-init . dynamic-fonts-setup))
 
 (use-package spaceline-config
   :straight spaceline
-  :config (spaceline-emacs-theme))
+  :functions spaceline-emacs-theme
+  :hook (after-init . spaceline-emacs-theme))
 
 (provide 'pluc-theme)
+
 ;;; pluc-theme.el ends here
