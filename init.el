b42d62f280a7cc2953975de8b2ab4990336a403a
Pierre-Luc Perrier <dev@the-pluc.net>
2020-12-24 12:45:37 +0100

    Add package `exec-path-from-shell`

--- init.el
+++ init.el
@@ -34,6 +34,16 @@
 (customize-set-variable 'use-package-verbose t)             ; Report about loading and configuration details
 (customize-set-variable 'use-package-compute-statistics t)  ; Report about loading and configuration details
 
+;; Ensure environment variables inside Emacs look the same as in the user's shell
+;; https://github.com/purcell/exec-path-from-shell
+(use-package exec-path-from-shell
+  :demand
+  :when (or (memq window-system '(mac ns x))
+            (daemonp))
+  :config
+  (exec-path-from-shell-copy-env "CPM_SOURCE_CACHE")
+  (exec-path-from-shell-initialize))
+
 ;; Customize mode lighters. use-package integration with `:diminish`.
 (use-package diminish :demand t)
 ;; Macros to define key bindings. use-package integration with `:bind`.
