56a2114167aa45e8c8be2b7d051ec1b45ebf9dfb
Pierre-Luc Perrier <dev@the-pluc.net>
2025-03-05 11:04:45 +0100

    lsp: add lsp-booster, warnings

--- abz/abz-lsp.el
+++ abz/abz-lsp.el
@@ -23,10 +23,9 @@
 (require 'use-package)
 
 (use-package lsp-ui
-  :functions
-  lsp-find-definition
-  :commands
-  lsp-ui-mode
+  :preface
+  (declare-function lsp-find-references "lsp-mode")
+  (declare-function lsp-find-definition "lsp-mode")
   :custom
   (lsp-ui-doc-enable t "Show information on hover")
   (lsp-ui-doc-show-with-cursor nil "Move the cursor over a symbol to show its documentation")
@@ -63,9 +62,39 @@
 (use-package which-key)
 
 (use-package lsp-mode
-  :commands lsp
+  :functions
+  lsp-register-client
+  make-lsp-client
+  lsp-tramp-connection
+  lsp-activate-on
+  :defines
+  lsp-clients-clangd-library-directories
   :init
   (setq read-process-output-max (* 3 1024 1024))
+  :preface
+  ;; https://github.com/blahgeek/emacs-lsp-booster
+  (defun lsp-booster--advice-json-parse (old-fn &rest args)
+    "Try to parse bytecode instead of json."
+    (or
+     (when (equal (following-char) ?#)
+       (let ((bytecode (read (current-buffer))))
+         (when (byte-code-function-p bytecode)
+           (funcall bytecode))))
+     (apply old-fn args)))
+  (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (if (and (not test?)                             ;; for check lsp-server-present?
+               (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+              (setcar orig-result command-from-exec-path))
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
   :custom
   (lsp-before-save-edits nil "Apply edits suggested by the language server before saving a document")
   (lsp-completion-enable t "Enable `completion-at-point' integration")
@@ -130,6 +159,13 @@
                     :remote? t
                     :server-id 'clangd-remote
                     :library-folders-fn (lambda (_workspace) lsp-clients-clangd-library-directories)))
+  (advice-add (if (progn (require 'json)
+                         (fboundp 'json-parse-buffer))
+                  'json-parse-buffer
+                'json-read)
+              :around
+              #'lsp-booster--advice-json-parse)
+  (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
   :hook
   ((c-mode-common . lsp)
    (cmake-mode . lsp)
